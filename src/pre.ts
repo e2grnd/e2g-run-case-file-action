7688f40b0eca95ade80efa66782d9b0afa229a57
Aaron Lampros <alampros@gmail.com>
2023-07-06 15:03:14 -0400

    remove pre script

--- src/pre.ts
+++
@@ -1,9 +0,0 @@
-import * as core from '@actions/core'
-import {exec} from 'child_process'
-
-exec('npm install -g rxjs', (err, stdout) => {
-  if (err) {
-    core.setFailed(err.message)
-  }
-  core.info(stdout)
-})
