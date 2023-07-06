c84e9bf435b28d7afc4f08b234939fe499a0ecc5
Aaron Lampros <alampros@gmail.com>
2023-07-06 13:06:00 -0400

    spawn => exec

--- src/pre.ts
+++ src/pre.ts
@@ -1,19 +1,9 @@
-import {spawn} from 'child_process'
+import * as core from '@actions/core'
+import {exec} from 'child_process'
 
-spawn('npm', ['install', 'rxjs'])
-
-// cmd.stdout.on('data', data => {
-//   console.log(`stdout: ${data}`)
-// })
-
-// cmd.stderr.on('data', data => {
-//   console.log(`stderr: ${data}`)
-// })
-
-// cmd.on('error', error => {
-//   console.log(`error: ${error.message}`)
-// })
-
-// cmd.on('close', code => {
-//   console.log(`child process exited with code ${code}`)
-// })
+exec('npm install -g rxjs', (err, stdout) => {
+  if (err) {
+    core.setFailed(err.message)
+  }
+  core.info(stdout)
+})
